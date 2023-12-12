cd3c642b5aa75523f5564d146ab7e729fa710920
Ethan Y. <theethan5753@gmail.com>
2023-12-11 22:17:31 -0800

    changed api file

--- api/index.py
+++ api/index.py
@@ -1,6 +1,30 @@
 from flask import Flask, Blueprint
-from api import api
 
 app = Flask(__name__)
+
+api = Blueprint('api', __name__)
+
+
+@api.route('/version')
+def version():
+    data = {
+        'data': "Ethan's API v1.0"
+    }
+    return data
+
+
+@api.route('/people')
+def people():
+    data = {
+        'data': [
+            'Ethan',
+            'James',
+            'Chris',
+            'John',
+            'Mario',
+        ]
+    }
+    return data
+
 
 app.register_blueprint(api, url_prefix='/api/v1')
